4639d7e2d26614fffdd0e1847a568ce886729f2e
unknown <pas.sergei2014@yandex.ru>
2021-03-21 15:12:13 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import VueRouter from 'vue-router'
 import Meta from 'vue-meta'
 import VueYandexMetrika from 'vue-yandex-metrika'
 import Home from '../views/Home.vue'
+import firebase from 'firebase/app'
 
 Vue.use(Meta, {
-  keyName: 'metaInfo',
-  attribute: 'data-vue-meta',
-  ssrAttribute: 'data-vue-meta-server-rendered',
-  tagIDKeyName: 'vmid',
-  refreshOnceOnNavigation: true
+	keyName: 'metaInfo',
+	attribute: 'data-vue-meta',
+	ssrAttribute: 'data-vue-meta-server-rendered',
+	tagIDKeyName: 'vmid',
+	refreshOnceOnNavigation: true
 }) 
 
 Vue.use(VueRouter)
@@ -28,6 +29,7 @@ const routes = [
 	{
 		path: '/personal_account',
 		name: 'PersonalAccount',
+		meta: {auth: true},
 		component: () => import('../views/PersonalAccount.vue')
 	}
 ]
@@ -49,11 +51,21 @@ const router = new VueRouter({
 
 
 Vue.use(VueYandexMetrika, {
-    id: 72354526,
-    router: router,
-    env: process.env.NODE_ENV
-    // other options
+	id: 72354526,
+	router: router,
+	env: process.env.NODE_ENV
+	// other options
 })
 
+router.beforeEach((to, from, next)=>{
+	const correntUser = firebase.auth().currentUser;
+	const requireAuth = to.matched.some(recored => record.meta.auth);
+
+	if (requireAuth && !currentUser) {
+		next('/login?massage=login');
+	} else {
+		next()
+	}
+})
 
 export default router
